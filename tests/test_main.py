a4942408be4deeba463b6d76e6dcbaf8e0399112
Quinten <quinten.roets@gmail.com>
2024-12-15 19:40:50 -0800

    mock test

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,9 +7,15 @@ from chat.main.main import main
 
 @patch("builtins.input", return_value="")
 @patch("chat.main.chat.Chat.send", side_effect=KeyboardInterrupt)
+@patch("chat.main.chat.Chat.show_reply")
 @pytest.mark.usefixtures("test_context")
-def test_main(mocked_input: MagicMock, mocked_send: MagicMock) -> None:
+def test_main(
+    mocked_input: MagicMock,
+    mocked_send: MagicMock,
+    mocked_reply: MagicMock,
+) -> None:
     with pytest.raises(KeyboardInterrupt):
         main()
     mocked_input.assert_called_once()
+    mocked_reply.assert_called_once()
     mocked_send.assert_called_once()
